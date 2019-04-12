620bcfd7ab121dffc66683006280012b6805a952
Technologeek <mcholythug@gmail.com>
2019-04-12 16:11:58 +0530

    added/baseBottomSheet

---
+++ app/src/main/java/com/socialpub/rahul/base/BaseBottomSheet.kt
@@ -0,0 +1,60 @@
+package com.socialpub.rahul.base
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.socialpub.rahul.utils.AppConst
+import com.socialpub.rahul.utils.dialogs.ProgressDialog
+
+abstract class BaseBottomSheet : BottomSheetDialogFragment() {
+
+    protected lateinit var attachedContext: Context
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        attachedContext = requireContext()
+    }
+
+    fun showProgress(message: String) {
+        val progressDialog = childFragmentManager.findFragmentByTag(AppConst.TAG_PROGRESS_DIALOG)
+        if (progressDialog == null) {
+            if (isAdded) {
+                val dialog = ProgressDialog.newInstance(message)
+                dialog.show(requireNotNull(childFragmentManager), AppConst.TAG_PROGRESS_DIALOG)
+            }
+        }
+    }
+
+    fun hideProgress() {
+        val progressDialog = childFragmentManager.findFragmentByTag(AppConst.TAG_PROGRESS_DIALOG)
+        if (progressDialog != null) {
+            (progressDialog as DialogFragment).dismiss()
+        }
+    }
+
+    protected abstract val contentLayout: Int
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        inflater.inflate(
+            contentLayout, container, false
+        )
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        isCancelable = false
+        setup(view)
+    }
+
+    protected abstract fun setup(view: View)
+
+    protected fun toast(msg: String) {
+        Toast.makeText(attachedContext, msg, Toast.LENGTH_SHORT).show()
+    }
+
+
+}
--- app/src/main/java/com/socialpub/rahul/ui/profile/edit/ProfileEditBottomSheet.kt
+++ app/src/main/java/com/socialpub/rahul/ui/profile/edit/ProfileEditBottomSheet.kt
@@ -1,39 +1,24 @@
 package com.socialpub.rahul.ui.profile.edit
 
 import android.content.Intent
-import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.DialogFragment
 import com.esafirm.imagepicker.features.ImagePicker
 import com.esafirm.imagepicker.features.ReturnMode
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.socialpub.rahul.R
+import com.socialpub.rahul.base.BaseBottomSheet
 import com.socialpub.rahul.ui.home.members.post.PostContract
-import com.socialpub.rahul.utils.AppConst
-import com.socialpub.rahul.utils.dialogs.ProgressDialog
 import com.squareup.picasso.Picasso
 import jp.wasabeef.picasso.transformations.CropCircleTransformation
 import kotlinx.android.synthetic.main.bottom_sheet_edit_profile.*
 
-class ProfileEditBottomSheet : BottomSheetDialogFragment(), ProfileEditContract.View {
+class ProfileEditBottomSheet : BaseBottomSheet(), ProfileEditContract.View {
 
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.bottom_sheet_edit_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        isCancelable = false
-        setup(view)
-    }
+    override val contentLayout: Int
+        get() = R.layout.bottom_sheet_edit_profile
 
     lateinit var controller: ProfileEditController
 
-    private fun setup(view: View) {
+    override fun setup(view: View) {
         controller = ProfileEditController(this)
         controller.onStart()
     }
@@ -65,8 +50,6 @@ class ProfileEditBottomSheet : BottomSheetDialogFragment(), ProfileEditContract.
         btn_update_profile.setOnClickListener { controller.updateUserProfile(edit_new_username.text.toString()) }
     }
 
-    fun toast(message: String) = Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
-
     override fun onError(message: String) = toast(message)
 
     override fun showSelectedImage(path: String) {
@@ -91,23 +74,6 @@ class ProfileEditBottomSheet : BottomSheetDialogFragment(), ProfileEditContract.
 
     companion object {
         fun newInstance() = ProfileEditBottomSheet()
-    }
-
-    fun showProgress(message: String) {
-        val progressDialog = childFragmentManager.findFragmentByTag(AppConst.TAG_PROGRESS_DIALOG)
-        if (progressDialog == null) {
-            if (isAdded) {
-                val dialog = ProgressDialog.newInstance(message)
-                dialog.show(requireNotNull(childFragmentManager), AppConst.TAG_PROGRESS_DIALOG)
-            }
-        }
-    }
-
-    fun hideProgress() {
-        val progressDialog = childFragmentManager.findFragmentByTag(AppConst.TAG_PROGRESS_DIALOG)
-        if (progressDialog != null) {
-            (progressDialog as DialogFragment).dismiss()
-        }
     }
 
 }
